7d34db7ea47903d27f7227a21626c95fc096f613
LucaVinciullo <vnclcu@hotmail.it>
2019-03-13 10:03:33 +0100

    Project Structure Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponentComponent } from './menu-component/menu-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { GameListComponentComponent } from './game-list-component/game-list-component.component';
+import { EditGameComponent } from './edit-game/edit-game.component';
+import { GameDetailComponent } from './game-detail/game-detail.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { GameListComponent } from './game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponentComponent,
+    HomeComponentComponent,
+    GameListComponentComponent,
+    EditGameComponent,
+    GameDetailComponent,
+    MenuComponent,
+    HomeComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule
