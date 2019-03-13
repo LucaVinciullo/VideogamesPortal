8e08893a5db719e95c8e1cf56d9950f569be5efe
LucaVinciullo <vnclcu@hotmail.it>
2019-03-13 10:08:11 +0100

    Compilation Error Structure Fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MenuComponentComponent } from './menu-component/menu-component.component';
-import { HomeComponentComponent } from './home-component/home-component.component';
-import { GameListComponentComponent } from './game-list-component/game-list-component.component';
 import { EditGameComponent } from './edit-game/edit-game.component';
 import { GameDetailComponent } from './game-detail/game-detail.component';
 import { MenuComponent } from './menu/menu.component';
@@ -14,9 +11,6 @@ import { GameListComponent } from './game-list/game-list.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponentComponent,
-    HomeComponentComponent,
-    GameListComponentComponent,
     EditGameComponent,
     GameDetailComponent,
     MenuComponent,
