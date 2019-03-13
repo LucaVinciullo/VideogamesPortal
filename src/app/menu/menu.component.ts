d185705d740402c874815f4ef1470d41d48202a6
LucaVinciullo <vnclcu@hotmail.it>
2019-03-13 17:49:53 +0100

    Removal of Detail from navigation menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,7 +10,7 @@ export class MenuComponent implements OnInit {
   menuList: MenuItem[] = [
     {id: 1, description: 'Home', selected: true},
     {id: 2, description: 'Lista', selected: false},
-    {id: 3, description: 'Detraglio', selected: false},
+    //{id: 3, description: 'Dettaglio', selected: false},
     {id: 4, description: 'Modifica', selected: false}
   ];
 
