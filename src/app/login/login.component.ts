8ce92c447968520c0d204eb85aaa16adf484c769
LucaVinciullo <vnclcu@hotmail.it>
2019-03-19 12:27:08 +0100

    Login page and property binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = '';
+
+
+  authenticate(): void{
+
+  }
 
   constructor() { }
 
   ngOnInit() {
+  
   }
 
 }
